e4449f96ba77775eed379aae99517dbf41f5f723
LC_Linkous <L.C.Linkous@gmail.com>
2022-08-21 18:14:38 -0400

    class-based update for Antenna Calculator. No major functionality changes. Can now run 'antenna_calculator.py' via command line or with default test vals in IDE

--- dxf_generator.py
+++ dxf_generator.py
@@ -1,40 +1,43 @@
-from antenna_calculator import args
+import ezdxf
 
-def generate_dxf(filename, W, L, x0, y0, Ws=None):
-    import ezdxf
+class dxfGenerator:
+    def __init__(self, args):
+        self.args = args
 
-    # Initialize drawing
-    doc = ezdxf.new('R2000')
+    def generate_dxf(self, filename, W, L, x0, y0, Ws=None):
 
-    # Add new entities to the modelspace:
-    msp = doc.modelspace()
+        # Initialize drawing
+        doc = ezdxf.new('R2000')
 
-    # Set up origin and supporting variables
-    substrate_origin = 0.0
-    originW = substrate_origin + 0.5 * W
-    originL = substrate_origin + 0.5 * L
+        # Add new entities to the modelspace:
+        msp = doc.modelspace()
 
-    if args.type == "microstrip":
-        g = Ws / 3
-        W_cut = (W - Ws - g * 2) / 2
+        # Set up origin and supporting variables
+        substrate_origin = 0.0
+        originW = substrate_origin + 0.5 * W
+        originL = substrate_origin + 0.5 * L
 
-    # Draw patch
-    if args.type == "microstrip":
-        points = [(originW, originL), (originW+W, originL), (originW+W, originL+L),
-            ((originW+W_cut + Ws + g * 2), originL+L), (originW+W_cut + Ws + g*2, originL+L-x0), (originW+W_cut + Ws + g, originL+L - x0),
-            (originW+W_cut + Ws + g, originL+L * 1.5), (originW+W_cut + g, originL+L * 1.5 ), (originW+W_cut + g, originL+L - x0),
-            (originW+W_cut, originL+L - x0), (originW + W_cut, originL+L), (originW, originL+L), (originW, originL)]
-        msp.add_lwpolyline(points)
-    elif args.type == "probe":
-        points = [(originW, originL), (originW+W, originL), (originW+W, originL+L), (originW, originL+L), (originW, originL)]
-        msp.add_lwpolyline(points)
-        msp.add_circle((originW+W-y0, originL+L-x0), radius=0.0005)    
-    
+        if self.args.type == "microstrip":
+            g = Ws / 3
+            W_cut = (W - Ws - g * 2) / 2
 
-    # Draw substrate
-    substrate_points = [(substrate_origin, substrate_origin), (substrate_origin+2*W, substrate_origin), 
-    (substrate_origin+2*W, substrate_origin+2*L), (substrate_origin, substrate_origin+2*L), (substrate_origin, substrate_origin)]
-    msp.add_lwpolyline(substrate_points)
+        # Draw patch
+        if self.args.type == "microstrip":
+            points = [(originW, originL), (originW+W, originL), (originW+W, originL+L),
+                ((originW+W_cut + Ws + g * 2), originL+L), (originW+W_cut + Ws + g*2, originL+L-x0), (originW+W_cut + Ws + g, originL+L - x0),
+                (originW+W_cut + Ws + g, originL+L * 1.5), (originW+W_cut + g, originL+L * 1.5 ), (originW+W_cut + g, originL+L - x0),
+                (originW+W_cut, originL+L - x0), (originW + W_cut, originL+L), (originW, originL+L), (originW, originL)]
+            msp.add_lwpolyline(points)
+        elif self.args.type == "probe":
+            points = [(originW, originL), (originW+W, originL), (originW+W, originL+L), (originW, originL+L), (originW, originL)]
+            msp.add_lwpolyline(points)
+            msp.add_circle((originW+W-y0, originL+L-x0), radius=0.0005)
 
-    # Save DXF file
-    doc.saveas(filename)
+
+        # Draw substrate
+        substrate_points = [(substrate_origin, substrate_origin), (substrate_origin+2*W, substrate_origin),
+        (substrate_origin+2*W, substrate_origin+2*L), (substrate_origin, substrate_origin+2*L), (substrate_origin, substrate_origin)]
+        msp.add_lwpolyline(substrate_points)
+
+        # Save DXF file
+        doc.saveas(filename)
